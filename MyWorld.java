777b87340874a661046f1c71b474c5ca979be519
Richard1669 <349966010@gapps.yrdsb.ca>
2025-04-28 09:11:00 -0400

    added apple and snake

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,12 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Snake snake = new Snake();
+        addObject(snake, 300, 200);
+        
+        Apple apple = new Apple();
+        addObject(apple, 300, 0);
+        
     }
 }
